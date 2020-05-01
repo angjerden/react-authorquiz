a22b5fb8b33c3186d94228c9a86926c472d22ede
Anders Gjerde <angjerden@gmail.com>
2020-05-01 19:49:01 +0200

    Added almost complete Form handling

---
+++ src/AuthorQuiz.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import AuthorQuiz from './AuthorQuiz';
+import ReactDOM from 'react-dom';
+
+describe("Author Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz />, div);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,102 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
+import AddAuthorForm from './AddAuthorForm';
+import {shuffle, sample} from 'underscore';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'img/Mark_Twain.jpg', 
+        imageSource: 'Wikimedia Commons',
+        books: [
+            'Huckleberry',
+            'Mississippi',
+            'Roughing'
+        ]
+    },
+    {
+        name: 'JK Rowling',
+        imageUrl: 'img/JKRowling.jpg', 
+        imageSource: 'Wikimedia Commons',
+        books: [
+            'Harry Potter',
+            'Harry Pothead',
+            'Happy Rotter'
+        ]
+    },
+    {
+        name: 'J R R Tolkien',
+        imageUrl: 'img/Tolkien.jpg', 
+        imageSource: 'Wikimedia Commons',
+        books: [
+            'Flu of a salesman',
+            'Lordy of thhe ringos',
+            'Tween of the stars of the temple maggots'
+        ]
+    },
+    {
+        name: 'Neil Gaiman',
+        imageUrl: 'img/NeilGaiman.jpg', 
+        imageSource: 'Wikimedia Commons',
+        books: [
+            'Discoworld',
+            'For whom the bell rings',
+            'It\'s turtles all the way down, Mr. Fry'
+        ]
+    },
+];
+
+function getTurnData(authors) {
+    const allBooks = authors.reduce((p, c, i) => {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => {
+            return author.books.some((title) => {
+                return title === answer;
+            })
+        })
+    };
+}
+
+const state = {
+    turnData: getTurnData(authors),
+    highlight: ''
+};
+
+function onAnswerSelected(answer) {
+    const isCorrect = state.turnData.author.books.some((book) => { return book === answer} );
+    state.highlight = isCorrect ? 'correct' : 'wrong';
+    render();
+}
+
+function App() {
+    return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>; 
+}
+
+function AuthorWrapper() {
+    return <AddAuthorForm onAddAuthor={console.log} />;
+}
+
+function render() {
+    ReactDOM.render(
+        <BrowserRouter>
+            <React.Fragment>
+                <Route exact path="/" component={App} />
+                <Route path="/add" component={AuthorWrapper}/>
+            </React.Fragment>
+        </BrowserRouter>
+    , document.getElementById('root'));
+}
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
